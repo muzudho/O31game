986cddd034e8a18217b0a9a1aab15f98ffcb7e35
むずでょ <muzudho1@gmail.com>
2022-09-27 23:23:41 +0900

    Computer turn

--- battle31.py
+++ battle31.py
@@ -1,41 +1,61 @@
+import random
+
+numbers_to_choose = []
+
+
 def main():
+    global numbers_to_choose
+
     print("")
     # 取っていい石の数（複数）
     numerics = input("""Rule how many can be taken?
 Example: 1,2,3
 > """).split(",")
-    numbers = [int(numeric) for numeric in numerics]
-    print(f"{numbers}")
+    numbers_to_choose = [int(numeric) for numeric in numerics]
+    print(f"{numbers_to_choose}")
 
     total = 31
 
-    print(create_position_text(total))
-
-    # Your turn.
     while True:
-        try:
-            choose = " ".join(numerics)
-            n = input(f"""How many do you take?
+        print(create_position_text(total))
+
+        # Your turn.
+        while True:
+            try:
+                choose = " ".join(numerics)
+                n = input(f"""How many do you take?
 Please choose: {choose}
 > """)
 
-            n = int(n)
-            if n in numbers:
-                # ループから抜ける
-                break
+                n = int(n)
+                if n in numbers_to_choose:
+                    # ループから抜ける
+                    break
 
-        except Exception as e:
-            print("Please try again!")
-            # print(e)
+            except Exception as e:
+                print("Please try again!")
+                # print(e)
 
-    total -= n
+        total -= n
 
-    pos_txt = create_position_text(total)
-    print(pos_txt)
+        print(create_position_text(total))
 
-    # Opponent turn.
+        if total < 1:
+            # 最後の石を取らされた
+            print("You lose!")
+            break
 
-    print("finishied")
+        # Opponent turn.
+        n = get_bestmove()
+        print(f"The computer took {n} stone(s).")
+        total -= n
+
+        if total < 1:
+            # 最後の石を取らせてやった
+            print("You win!")
+            break
+
+    # finished.
 
 
 def create_position_text(total):
@@ -51,6 +71,13 @@ def create_position_text(total):
 """
 
     return s
+
+
+def get_bestmove():
+    """コンピューターの選んだ数"""
+    global numbers_to_choose
+    num = random.choice(numbers_to_choose)
+    return num
 
 
 if __name__ == "__main__":
