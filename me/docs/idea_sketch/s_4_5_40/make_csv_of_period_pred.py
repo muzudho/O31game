86b9729d463db9689a373638016c3ce838c0f5a5
むずでょ <muzudho1@gmail.com>
2022-10-01 00:50:25 +0900

    調べものを開始（＾～＾）

--- me/docs/idea_sketch/s_4_5_40/make_csv_of_period_pred.py
+++ me/docs/idea_sketch/s_4_5_40/make_csv_of_period_pred.py
@@ -1,7 +1,7 @@
 """
 cd me/docs/idea_sketch/s_4_5_40
 
-python.exe make_pred_list.py
+python.exe make_csv_of_period_pred.py
 
 Explanation
 ===========
@@ -10,6 +10,11 @@ Explanation
 from make_c_from_a_b import get_3_periods_given_a_b_c
 
 if __name__ == "__main__":
+
+    text = ""
+    text += "a,b,c,p1,p2,p3,correct\n"
+    """S = { a, b, c } Period predict: p1, p2, p3 and Correct period"""
+
     for a in range(1, 10):
         for b in range(a+1, a+1+10):
             for z in range(1, 11):  # ab の定数倍
@@ -18,5 +23,7 @@ if __name__ == "__main__":
                 a_p_b, b_p_c, a_p_c = get_3_periods_given_a_b_c(a, b, c)
                 """S = { a, b, c } を渡すと、周期の候補を３つ返す"""
 
-                print(
-                    f"S = {{ {a:2}, {b:2}, {c:3} }}  Period: Maybe    {a_p_b}    {b_p_c}    {a_p_c}")
+                text += f"{a},{b},{c},{a_p_b},{b_p_c},{a_p_c},-1\n"
+
+    with open('./period_pred_tmp.csv', 'w', encoding="utf-8") as f:
+        f.write(text)
