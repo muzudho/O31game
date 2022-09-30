22a4cbf9d5223f1ec537394573a11a1af96a0e36
むずでょ <muzudho1@gmail.com>
2022-10-01 00:25:08 +0900

    関数の形を変えてみる

--- me/docs/idea_sketch/s_4_5_40/make_c_from_a_b.py
+++ me/docs/idea_sketch/s_4_5_40/make_c_from_a_b.py
@@ -7,13 +7,11 @@ Explanation
 ===========
 S = { a, b, c }
 の、いい感じの a, b, c を選んでくれるアルゴリズム。
-
-a と b を選べば、 いい感じの c を勝手に選んでくれる
 """
 
 
-def make_c_from_a_b(a, b, c):
-    """
+def get_3_periods_given_a_b_c(a, b, c):
+    """S = { a, b, c } を渡すと、周期の候補を３つ返します。
     やりたいことは次の通り
 
         c = ax
@@ -28,11 +26,15 @@ def make_c_from_a_b(a, b, c):
     よくみたら　この式、 c = abz なんじゃないのか。 x も y も要らないや
     """
 
-    ab = a * b
-    a_p_b = a + b
-    b_p_c = b + c
-    a_p_c = a + c
+    # 全ての組み合わせについて、足すだけじゃないか（＾～＾）
+    return a + b, b + c, a + c
 
+
+def choose_period_with_guess(a_p_b, b_p_c, a_p_c):
+    """推測で周期を選ぶ。
+    周期の候補を３つ受け取るが、どれが本物なのか、分かんないんで、とりあえず　当てずっぽうで決めてみる。
+    しかし、取り方が３種類あるので、結局３つ返す"""
+    ab = a * b
     loop_length = 3 * a * b
     c_mod_loop_length = c % loop_length
     feeling = c_mod_loop_length // ab
@@ -54,11 +56,12 @@ def make_c_from_a_b(a, b, c):
         else:
             raise ValueError(f"unexpected feeling:{feeling}")
 
-    print(
-        f"Example: S={{{a}, {b}, {c}}} The period the program predicts:  {a_p_b}  or  {b_p_c}  or  {a_p_c}.    May come off but maybe: (A) {maybe_numbers[0]}   (B) {maybe_numbers[1]}   (C) {maybe_numbers[2]}.")
+    return maybe_numbers[0], maybe_numbers[1], maybe_numbers[2]
 
 
 if __name__ == "__main__":
+    """a と b を選べば、 いい感じの c を勝手に選んでくれる"""
+
     enter = input("""S = { a, b, c }
 Please enter a and b. This program chooses a nice looking c.
 Example: S=4 5
@@ -72,4 +75,13 @@ S=""")
         """z は、 ab の定数倍を意味している。いくつも候補を出す"""
         c = a * b * z
 
-        make_c_from_a_b(a, b, c)
+        a_p_b, b_p_c, a_p_c = get_3_periods_given_a_b_c(a, b, c)
+        """S = { a, b, c } を渡すと、周期の候補を３つ返す"""
+
+        period1, period2, period3 = choose_period_with_guess(
+            a_p_b, b_p_c, a_p_c)
+        """周期の候補が３つ返ってくるが、どれが本物なのか、分かんないんで、とりあえず　当てずっぽうで決めてみる。
+        しかし、取り方が３種類あるので、結局３つ返す"""
+
+        print(
+            f"Example: S={{{a}, {b}, {c}}} The period the program predicts:  {a_p_b}  or  {b_p_c}  or  {a_p_c}.    May come off but maybe: (A) {period1}   (B) {period2}   (C) {period3}.")
