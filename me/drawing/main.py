9ae4d3565673f52238a630073a0034f60ef87017
むずでょ <muzudho1@gmail.com>
2022-10-04 22:24:03 +0900

    しらべもの

--- me/drawing/main.py
+++ me/drawing/main.py
@@ -15,69 +15,73 @@ len_Nz = 200
 """とりあえず c の２倍はある要素数。画像の横幅にも使われるので大きすぎないように
 画像ファイルは、この半分ぐらいの部分で作ります"""
 
-gen_s_a_b_c_image(a=1, b=2, c=3, len_Nz=len_Nz, zoom=zoom)
-gen_s_a_b_c_image(a=1, b=3, c=5, len_Nz=len_Nz, zoom=zoom)
-gen_s_a_b_c_image(a=1, b=4, c=20, len_Nz=len_Nz, zoom=zoom)
-gen_s_a_b_c_image(a=1, b=6, c=30, len_Nz=len_Nz, zoom=zoom)
-gen_s_a_b_c_image(a=1, b=2, c=4, len_Nz=len_Nz, zoom=zoom)
 
-gen_s_a_b_c_image(a=1, b=3, c=7, len_Nz=len_Nz, zoom=zoom)
-"""S={a,b,c} として、 S={1, 2a+1, 2b+1}"""
+def main():
+    # 3和音シリーズ
+    for ms in range(0, 12):
+        """Music scale
+        1: C
+        2: C#
+        3: D
+        4: D#
+        5: E
+        6: F
+        7: F#
+        8: G
+        9: G#
+        10: A
+        11: A#
+        12: B
+        """
+        gen_s_a_b_c_image(a=1+ms, b=5+ms, c=8+ms, len_Nz=len_Nz, zoom=zoom)
+        """メジャーコード"""
 
-gen_s_a_b_c_image(a=1, b=3, c=4, len_Nz=len_Nz, zoom=zoom)
-gen_s_a_b_c_image(a=1, b=4, c=7, len_Nz=len_Nz, zoom=zoom)
+        gen_s_a_b_c_image(a=1+ms, b=4+ms, c=8+ms, len_Nz=len_Nz, zoom=zoom)
+        """m マイナーコード"""
 
-gen_s_a_b_c_image(a=4, b=9, c=19, len_Nz=len_Nz, zoom=zoom)
+        gen_s_a_b_c_image(a=1+ms, b=6+ms, c=8+ms, len_Nz=len_Nz, zoom=zoom)
+        """sus4 サスフォー"""
 
-# 3和音シリーズ
-gen_s_a_b_c_image(a=1, b=5, c=8, len_Nz=len_Nz, zoom=zoom)
-"""C シーメジャーコード"""
+        gen_s_a_b_c_image(a=1+ms, b=5+ms, c=7+ms, len_Nz=len_Nz, zoom=zoom)
+        """-5 フラットファイブ"""
 
-gen_s_a_b_c_image(a=1, b=4, c=8, len_Nz=len_Nz, zoom=zoom)
-"""Cm シーマイナーコード"""
+        gen_s_a_b_c_image(a=1+ms, b=5+ms, c=9+ms, len_Nz=len_Nz, zoom=zoom)
+        """aug オーギュメント"""
 
-gen_s_a_b_c_image(a=1, b=6, c=8, len_Nz=len_Nz, zoom=zoom)
-"""Csus4 シーサスフォー"""
+        gen_s_a_b_c_image(a=1+ms, b=4+ms, c=7+ms, len_Nz=len_Nz, zoom=zoom)
+        """dim ディミニッシュ"""
 
-gen_s_a_b_c_image(a=1, b=5, c=7, len_Nz=len_Nz, zoom=zoom)
-"""C-5 シーフラットファイブ"""
 
-gen_s_a_b_c_image(a=1, b=5, c=9, len_Nz=len_Nz, zoom=zoom)
-"""Caug シーオーギュメント"""
+def gen_test():
+    gen_s_a_b_c_image(a=1, b=2, c=3, len_Nz=len_Nz, zoom=zoom)
+    gen_s_a_b_c_image(a=1, b=3, c=5, len_Nz=len_Nz, zoom=zoom)
+    gen_s_a_b_c_image(a=1, b=4, c=20, len_Nz=len_Nz, zoom=zoom)
+    gen_s_a_b_c_image(a=1, b=6, c=30, len_Nz=len_Nz, zoom=zoom)
+    gen_s_a_b_c_image(a=1, b=2, c=4, len_Nz=len_Nz, zoom=zoom)
 
-gen_s_a_b_c_image(a=1, b=4, c=7, len_Nz=len_Nz, zoom=zoom)
-"""Cdim シーディミニッシュ"""
+    gen_s_a_b_c_image(a=1, b=3, c=7, len_Nz=len_Nz, zoom=zoom)
+    """S={a,b,c} として、 S={1, 2a+1, 2b+1}"""
 
-gen_s_a_b_c_image(a=2, b=6, c=9, len_Nz=len_Nz, zoom=zoom)
-"""C# シーシャープメジャーコード"""
+    gen_s_a_b_c_image(a=1, b=3, c=4, len_Nz=len_Nz, zoom=zoom)
+    gen_s_a_b_c_image(a=1, b=4, c=7, len_Nz=len_Nz, zoom=zoom)
 
-gen_s_a_b_c_image(a=2, b=5, c=9, len_Nz=len_Nz, zoom=zoom)
-"""C#m シーシャープマイナーコード"""
+    gen_s_a_b_c_image(a=4, b=9, c=19, len_Nz=len_Nz, zoom=zoom)
 
-gen_s_a_b_c_image(a=2, b=7, c=9, len_Nz=len_Nz, zoom=zoom)
-"""C#sus4 シーシャープサスフォー"""
 
-gen_s_a_b_c_image(a=2, b=6, c=8, len_Nz=len_Nz, zoom=zoom)
-"""C#-5 シーシャープフラットファイブ"""
+def gen_random():
+    for i in range(0, 100):
+        a = random.randint(1, 10)
+        if 97 < a:
+            a = 97
+        b = a + random.randint(1, 10)
+        if 98 < b:
+            b = 98
+        c = b + random.randint(1, 10)
+        if 99 < c:
+            c = 99
 
-gen_s_a_b_c_image(a=2, b=6, c=10, len_Nz=len_Nz, zoom=zoom)
-"""C#aug シーシャープオーギュメント"""
+        gen_s_a_b_c_image(a=a, b=b, c=c, len_Nz=len_Nz, zoom=zoom)
 
-gen_s_a_b_c_image(a=2, b=5, c=8, len_Nz=len_Nz, zoom=zoom)
-"""C#dim シーシャープディミニッシュ"""
 
-gen_s_a_b_c_image(a=3, b=7, c=10, len_Nz=len_Nz, zoom=zoom)
-"""D ディーメジャーコード"""
-
-for i in range(0, 100):
-    a = random.randint(1, 10)
-    if 97 < a:
-        a = 97
-    b = a + random.randint(1, 10)
-    if 98 < b:
-        b = 98
-    c = b + random.randint(1, 10)
-    if 99 < c:
-        c = 99
-
-    gen_s_a_b_c_image(a=a, b=b, c=c, len_Nz=len_Nz, zoom=zoom)
+if __name__ == "__main__":
+    main()
