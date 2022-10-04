febc3b81bb91713e355f3c9fa43338e9dcb3e8cd
むずでょ <muzudho1@gmail.com>
2022-10-04 21:43:22 +0900

    しらべもの

--- me/drawing/main.py
+++ me/drawing/main.py
@@ -17,7 +17,57 @@ len_Nz = 200
 
 gen_s_a_b_c_image(a=1, b=2, c=3, len_Nz=len_Nz, zoom=zoom)
 gen_s_a_b_c_image(a=1, b=3, c=5, len_Nz=len_Nz, zoom=zoom)
+gen_s_a_b_c_image(a=1, b=4, c=20, len_Nz=len_Nz, zoom=zoom)
+gen_s_a_b_c_image(a=1, b=6, c=30, len_Nz=len_Nz, zoom=zoom)
+gen_s_a_b_c_image(a=1, b=2, c=4, len_Nz=len_Nz, zoom=zoom)
+
+gen_s_a_b_c_image(a=1, b=3, c=7, len_Nz=len_Nz, zoom=zoom)
+"""S={a,b,c} として、 S={1, 2a+1, 2b+1}"""
+
+gen_s_a_b_c_image(a=1, b=3, c=4, len_Nz=len_Nz, zoom=zoom)
+gen_s_a_b_c_image(a=1, b=4, c=7, len_Nz=len_Nz, zoom=zoom)
+
 gen_s_a_b_c_image(a=4, b=9, c=19, len_Nz=len_Nz, zoom=zoom)
+
+# 3和音シリーズ
+gen_s_a_b_c_image(a=1, b=5, c=8, len_Nz=len_Nz, zoom=zoom)
+"""C シーメジャーコード"""
+
+gen_s_a_b_c_image(a=1, b=4, c=8, len_Nz=len_Nz, zoom=zoom)
+"""Cm シーマイナーコード"""
+
+gen_s_a_b_c_image(a=1, b=6, c=8, len_Nz=len_Nz, zoom=zoom)
+"""Csus4 シーサスフォー"""
+
+gen_s_a_b_c_image(a=1, b=5, c=7, len_Nz=len_Nz, zoom=zoom)
+"""C-5 シーフラットファイブ"""
+
+gen_s_a_b_c_image(a=1, b=5, c=9, len_Nz=len_Nz, zoom=zoom)
+"""Caug シーオーギュメント"""
+
+gen_s_a_b_c_image(a=1, b=4, c=7, len_Nz=len_Nz, zoom=zoom)
+"""Cdim シーディミニッシュ"""
+
+gen_s_a_b_c_image(a=2, b=6, c=9, len_Nz=len_Nz, zoom=zoom)
+"""C# シーシャープメジャーコード"""
+
+gen_s_a_b_c_image(a=2, b=5, c=9, len_Nz=len_Nz, zoom=zoom)
+"""C#m シーシャープマイナーコード"""
+
+gen_s_a_b_c_image(a=2, b=7, c=9, len_Nz=len_Nz, zoom=zoom)
+"""C#sus4 シーシャープサスフォー"""
+
+gen_s_a_b_c_image(a=2, b=6, c=8, len_Nz=len_Nz, zoom=zoom)
+"""C#-5 シーシャープフラットファイブ"""
+
+gen_s_a_b_c_image(a=2, b=6, c=10, len_Nz=len_Nz, zoom=zoom)
+"""C#aug シーシャープオーギュメント"""
+
+gen_s_a_b_c_image(a=2, b=5, c=8, len_Nz=len_Nz, zoom=zoom)
+"""C#dim シーシャープディミニッシュ"""
+
+gen_s_a_b_c_image(a=3, b=7, c=10, len_Nz=len_Nz, zoom=zoom)
+"""D ディーメジャーコード"""
 
 for i in range(0, 100):
     a = random.randint(1, 10)
