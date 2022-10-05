307bc8063d9ad6bcf2d7c9e0ec3ecc87b2c87499
むずでょ <muzudho1@gmail.com>
2022-10-05 22:26:07 +0900

    gear

--- me/ideas/gear_with_moving_hole/main.py
+++ me/ideas/gear_with_moving_hole/main.py
@@ -14,31 +14,27 @@ def main():
     c = random.randint(b+0, b+10)
     print(f"S={{a,b,c}} : S={{{a}, {b}, {c}}}")
 
+    # n mod a + n mod b + n mod c の略
     go_gear("a + b + c", a, b, c)  # a.b.c
     go_gear("a + b - c", a, b, c)
     go_gear("a - b + c", a, b, c)
     go_gear("a - b - c", a, b, c)
-
     go_gear("a + c + b", a, c, b)  # a.c.b
     go_gear("a + c - b", a, c, b)
     go_gear("a - c + b", a, c, b)
     go_gear("a - c - b", a, c, b)
-
     go_gear("b + a + c", b, a, c)  # b.a.c
     go_gear("b + a - c", b, a, c)
     go_gear("b - a + c", b, a, c)
     go_gear("b - a - c", b, a, c)
-
     go_gear("b + c + a", b, c, a)  # b.c.a
     go_gear("b + c - a", b, c, a)
     go_gear("b - c + a", b, c, a)
     go_gear("b - c - a", b, c, a)
-
     go_gear("c + a + b", c, a, b)  # c.a.b
     go_gear("c + a - b", c, a, b)
     go_gear("c - a + b", c, a, b)
     go_gear("c - a - b", c, a, b)
-
     go_gear("c + b + a", c, b, a)  # c.b.a
     go_gear("c + b - a", c, b, a)
     go_gear("c - b + a", c, b, a)
@@ -48,23 +44,21 @@ def main():
 def go_gear(expression, a, b, c):
     """n1 op1 n2 op2 op3"""
     tokens = expression.split(" ")
-    n1 = choise_operand(tokens[0], a, b, c)
-    n2 = choise_operand(tokens[2], a, b, c)
-    n3 = choise_operand(tokens[4], a, b, c)
+    m1 = choise_operand(tokens[0], a, b, c)
+    m2 = choise_operand(tokens[2], a, b, c)
+    m3 = choise_operand(tokens[4], a, b, c)
     print(f"""
-n {tokens[0]:2} mod 3 {tokens[1]} {tokens[2]:2} mod 5 {tokens[3]} {tokens[4]:2} mod 7
+n n mod {m1:>2} {tokens[1]} n mod {m2:>2} {tokens[3]} n mod {m3:>2}
   --------   --------   --------   -----""")
-    m1 = 3
-    m2 = 5
-    m3 = 7
 
-    l1 = n1 % m1
-    l2 = n2 % m2
-    l3 = n3 % m3
-    value = do_operator(l1, tokens[1], l2)
-    value = do_operator(value, tokens[3], l3)
-    print(
-        f"n {l1:8} {tokens[1]} {l2:8} {tokens[3]} {l3:8} = {value:5}\n")
+    for n in range(0, 40):
+        l1 = n % m1
+        l2 = n % m2
+        l3 = n % m3
+        value = do_operator(l1, tokens[1], l2)
+        value = do_operator(value, tokens[3], l3)
+        print(
+            f"n {l1:8} {tokens[1]} {l2:8} {tokens[3]} {l3:8} = {value:5}")
 
 
 def choise_operand(expected, a, b, c):
