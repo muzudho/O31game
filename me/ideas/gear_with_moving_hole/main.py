088226464a30d85d3bf25ae3a3e1e5bda31821b4
むずでょ <muzudho1@gmail.com>
2022-10-05 22:37:46 +0900

    gear

--- me/ideas/gear_with_moving_hole/main.py
+++ me/ideas/gear_with_moving_hole/main.py
@@ -5,14 +5,17 @@ python.exe -m ideas.gear_with_moving_hole.main
 """
 import random
 
+a = 5
+b = 7
+c = 13
+len_Nz = 40
+expected_grundy_seq = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+                       2, 2, 0, 2, 2, 3, 3, 1, 0, 3, 0, 0, 0, 1, 0, 1, 1, 1, 2, 1, 0, 2, 0, 3, 2, 1, 0, 1, 0, 3, ]
+
 
 def main():
+    global a, b, c, len_Nz, expected_grundy_seq
     print("Gear with moving hole")
-
-    a = 5
-    b = 7
-    c = 13
-    len_Nz = 40
     print(f"S={{a,b,c}} : S={{{a}, {b}, {c}}}")
 
     # n mod a + n mod b + n mod c の略
@@ -44,6 +47,8 @@ def main():
 
 def go_gear(expression, a, b, c, len_Nz):
     """n1 op1 n2 op2 op3"""
+    global expected_grundy_seq
+
     tokens = expression.split(" ")
     m1 = choise_operand(tokens[0], a, b, c)
     m2 = choise_operand(tokens[2], a, b, c)
@@ -52,6 +57,9 @@ def go_gear(expression, a, b, c, len_Nz):
 n n mod {m1:>2} {tokens[1]} n mod {m2:>2} {tokens[3]} n mod {m3:>2}   Grundy
   --------   --------   --------   ------""")
 
+    is_perfect = True
+
+    i = 0
     for n in range(0, len_Nz):
         l1 = n % m1
         l2 = n % m2
@@ -61,6 +69,14 @@ n n mod {m1:>2} {tokens[1]} n mod {m2:>2} {tokens[3]} n mod {m3:>2}   Grundy
         value = value % 4  # 0～3 にしたい
         print(
             f"n {l1:8} {tokens[1]} {l2:8} {tokens[3]} {l3:8} = {value:6}")
+
+        if expected_grundy_seq[i] != value:
+            is_perfect = False
+
+        i += 1
+
+    if is_perfect:
+        raise ValueError("Oh, my God!")
 
 
 def choise_operand(expected, a, b, c):
