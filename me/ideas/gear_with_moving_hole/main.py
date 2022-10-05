640116fca72a95cb4868823e2444612cd4fe6a10
むずでょ <muzudho1@gmail.com>
2022-10-05 22:46:27 +0900

    gear

--- me/ideas/gear_with_moving_hole/main.py
+++ me/ideas/gear_with_moving_hole/main.py
@@ -21,37 +21,42 @@ def main():
         b = random.randint(a+1, a+100)
         c = random.randint(b+1, b+100)
 
+        # 係数。線形ではないだろうし
+        aa = random.randint(0, 10)
+        bb = random.randint(0, 10)
+        cc = random.randint(0, 10)
+
         print("Gear with moving hole")
         print(f"S={{a,b,c}} : S={{{a}, {b}, {c}}}")
 
-        # n mod a + n mod b + n mod c の略
-        go_gear("a + b + c", a, b, c, len_Nz)  # a.b.c
-        go_gear("a + b - c", a, b, c, len_Nz)
-        go_gear("a - b + c", a, b, c, len_Nz)
-        go_gear("a - b - c", a, b, c, len_Nz)
-        go_gear("a + c + b", a, c, b, len_Nz)  # a.c.b
-        go_gear("a + c - b", a, c, b, len_Nz)
-        go_gear("a - c + b", a, c, b, len_Nz)
-        go_gear("a - c - b", a, c, b, len_Nz)
-        go_gear("b + a + c", b, a, c, len_Nz)  # b.a.c
-        go_gear("b + a - c", b, a, c, len_Nz)
-        go_gear("b - a + c", b, a, c, len_Nz)
-        go_gear("b - a - c", b, a, c, len_Nz)
-        go_gear("b + c + a", b, c, a, len_Nz)  # b.c.a
-        go_gear("b + c - a", b, c, a, len_Nz)
-        go_gear("b - c + a", b, c, a, len_Nz)
-        go_gear("b - c - a", b, c, a, len_Nz)
-        go_gear("c + a + b", c, a, b, len_Nz)  # c.a.b
-        go_gear("c + a - b", c, a, b, len_Nz)
-        go_gear("c - a + b", c, a, b, len_Nz)
-        go_gear("c - a - b", c, a, b, len_Nz)
-        go_gear("c + b + a", c, b, a, len_Nz)  # c.b.a
-        go_gear("c + b - a", c, b, a, len_Nz)
-        go_gear("c - b + a", c, b, a, len_Nz)
-        go_gear("c - b - a", c, b, a, len_Nz)
+        # n aa mod a + n bb mod b + n cc mod c の略
+        go_gear("a + b + c", a, b, c, aa, bb, cc, len_Nz)  # a.b.c
+        go_gear("a + b - c", a, b, c, aa, bb, cc, len_Nz)
+        go_gear("a - b + c", a, b, c, aa, bb, cc, len_Nz)
+        go_gear("a - b - c", a, b, c, aa, bb, cc, len_Nz)
+        go_gear("a + c + b", a, c, b, aa, cc, bb, len_Nz)  # a.c.b
+        go_gear("a + c - b", a, c, b, aa, cc, bb, len_Nz)
+        go_gear("a - c + b", a, c, b, aa, cc, bb, len_Nz)
+        go_gear("a - c - b", a, c, b, aa, cc, bb, len_Nz)
+        go_gear("b + a + c", b, a, c, bb, aa, cc, len_Nz)  # b.a.c
+        go_gear("b + a - c", b, a, c, bb, aa, cc, len_Nz)
+        go_gear("b - a + c", b, a, c, bb, aa, cc, len_Nz)
+        go_gear("b - a - c", b, a, c, bb, aa, cc, len_Nz)
+        go_gear("b + c + a", b, c, a, bb, cc, aa, len_Nz)  # b.c.a
+        go_gear("b + c - a", b, c, a, bb, cc, aa, len_Nz)
+        go_gear("b - c + a", b, c, a, bb, cc, aa, len_Nz)
+        go_gear("b - c - a", b, c, a, bb, cc, aa, len_Nz)
+        go_gear("c + a + b", c, a, b, cc, aa, bb, len_Nz)  # c.a.b
+        go_gear("c + a - b", c, a, b, cc, aa, bb, len_Nz)
+        go_gear("c - a + b", c, a, b, cc, aa, bb, len_Nz)
+        go_gear("c - a - b", c, a, b, cc, aa, bb, len_Nz)
+        go_gear("c + b + a", c, b, a, cc, bb, aa, len_Nz)  # c.b.a
+        go_gear("c + b - a", c, b, a, cc, bb, aa, len_Nz)
+        go_gear("c - b + a", c, b, a, cc, bb, aa, len_Nz)
+        go_gear("c - b - a", c, b, a, cc, bb, aa, len_Nz)
 
 
-def go_gear(expression, a, b, c, len_Nz):
+def go_gear(expression, a, b, c, aa, bb, cc, len_Nz):
     """n1 op1 n2 op2 op3"""
     global expected_grundy_seq
 
@@ -59,17 +64,20 @@ def go_gear(expression, a, b, c, len_Nz):
     m1 = choise_operand(tokens[0], a, b, c)
     m2 = choise_operand(tokens[2], a, b, c)
     m3 = choise_operand(tokens[4], a, b, c)
+    mm1 = choise_operand(tokens[0], aa, bb, cc)
+    mm2 = choise_operand(tokens[2], aa, bb, cc)
+    mm3 = choise_operand(tokens[4], aa, bb, cc)
     print(f"""
-n n mod {m1:>2} {tokens[1]} n mod {m2:>2} {tokens[3]} n mod {m3:>2}   Grundy
-  --------   --------   --------   ------""")
+n {mm1:>2}n mod {m1:>2} {tokens[1]} {mm2:>2}n mod {m2:>2} {tokens[3]} {mm3:>2}n mod {m3:>2}   Grundy
+  ----------   ----------   ----------   ------""")
 
     is_perfect = True
 
     i = 0
     for n in range(0, len_Nz):
-        l1 = n % m1
-        l2 = n % m2
-        l3 = n % m3
+        l1 = mm1 * n % m1
+        l2 = mm2 * n % m2
+        l3 = mm3 * n % m3
         value = do_operator(l1, tokens[1], l2)
         value = do_operator(value, tokens[3], l3)
         value = value % 4  # 0～3 にしたい
