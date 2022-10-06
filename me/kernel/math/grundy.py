bfc3def743fe67a775e47c53cd2eda18d85bf93c
むずでょ <muzudho1@gmail.com>
2022-10-07 08:42:56 +0900

    WIP dame coloring

--- me/kernel/math/grundy.py
+++ me/kernel/math/grundy.py
@@ -57,16 +57,23 @@ class GrundySequence:
 
             grundy_list[i] = grundy
 
-        return GrundySequence(grundy_list)
+        return GrundySequence(grundy_list, S_list)
 
-    def __init__(self, grundy_list: list):
+    def __init__(self, grundy_list: list, S_list: list):
 
         self.__grundy_list = grundy_list
         """添え字は、盤上の石の数 n （ 0 ～ len(N) ）"""
 
+        self.__S_list = S_list
+        """サブストラクションセット"""
+
     @property
     def len(self):
         return len(self.__grundy_list)
+
+    @property
+    def S_list(self):
+        return self.__S_list
 
     def get_grundy_at(self, i: int):
         """グランディ数
@@ -100,29 +107,48 @@ class DameColorSequence():
         len_Nz = grundy_sequence.len
         dame_color_list = [0] * len_Nz
 
-        current_eye_color = 0
+        adjacent_eye_color = 0  # 黒
 
         # 往路
         for i in range(0, len_Nz):
             grundy_num = grundy_sequence.get_grundy_at(i)
 
-            if grundy_num != 0:
-                current_eye_color = grundy_num
+            if grundy_num != 0:  # 石
+                if i % grundy_sequence.S_list[2] == 0:
+                    eye_color = 3  # 青
+                elif i % grundy_sequence.S_list[1] == 0:
+                    eye_color = 2  # 緑
+                elif i % grundy_sequence.S_list[0] == 0:
+                    eye_color = 1  # 赤
+                else:
+                    eye_color = 0  # 黒
+
+                adjacent_eye_color = eye_color
+            else:  # 目
+                dame_color_list[i] = adjacent_eye_color
 
         # 復路
-        for i in range(0, len_Nz):
+        for i in range(1, len_Nz):
             rev_i = len_Nz - i - 1
+
             grundy_num = grundy_sequence.get_grundy_at(rev_i)
 
-            if grundy_num != 0:
-                current_eye_color = grundy_num
-            else:
-                dame_color = dame_color_list[rev_i]
-                if dame_color != current_eye_color:
-                    """駄目"""
-                    dame_color_list[rev_i] = 0
+            if grundy_num != 0:  # 石
+                if rev_i % grundy_sequence.S_list[2] == 0:
+                    eye_color = 3  # 青
+                elif rev_i % grundy_sequence.S_list[1] == 0:
+                    eye_color = 2  # 緑
+                elif rev_i % grundy_sequence.S_list[0] == 0:
+                    eye_color = 1  # 赤
+                else:
+                    eye_color = 0  # 黒
 
-            dame_color_list[i] = current_eye_color
+                adjacent_eye_color = eye_color
+            else:  # 目
+                dame_color = dame_color_list[rev_i]
+                if dame_color != adjacent_eye_color:
+                    """駄目"""
+                    dame_color_list[rev_i] = 0  # 黒
 
         return DameColorSequence(dame_color_list)
 
