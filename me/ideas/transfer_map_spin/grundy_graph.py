b0b10d54627b9650aff1e79e17ba52346805e9cb
むずでょ <muzudho1@gmail.com>
2022-10-08 21:23:04 +0900

    spin

--- me/ideas/transfer_map_spin/grundy_graph.py
+++ me/ideas/transfer_map_spin/grundy_graph.py
@@ -1,8 +1,75 @@
 from ideas.transfer_map_spin.transposition_table import TranspositionTable
 from ideas.transfer_map_spin.transposition_color_table import TranspositionColorTable
+from ideas.transfer_map_spin.trident_hair import TridentHair
+from ideas.transfer_map_spin.nim_constants import nim_constants
 
 
 class GrundyGraph:
+
+    @staticmethod
+    def make(drawing_columns, drawing_rows, a, b, c, ha, hb, hc):
+
+        ins = GrundyGraph()
+
+        GrundyGraph.__make_each_tridents_from(
+            ins, drawing_columns, drawing_rows, a, b, c, ha, hb, hc,
+            ins.root_point, ins.tp_table, nim_constants.stonecolor_x, ins.src_color_table)
+        return ins
+
+    @staticmethod
+    def __make_each_tridents_from(ins, drawing_columns, drawing_rows, a, b, c, ha, hb, hc, src_point, tp_table, src_stonecolor, src_color_table):
+        trident = TridentHair.make(
+            src_point,
+            columns=drawing_columns,
+            rows=drawing_rows,
+            a=a,
+            b=b,
+            c=c,
+            ha=ha,
+            hb=hb,
+            hc=hc)
+
+        if trident is not None:
+            """指定の範囲内のみ描画"""
+
+            hash_key = trident.create_hash()
+            print(f"src({trident.src_point}) hash_key:{hash_key}")
+
+            if not tp_table.contains_key(hash_key):
+                """存在しない三本毛なら登録"""
+                tp_table.add_trident(hash_key, trident)
+                src_color_table.add_stonecolor(
+                    trident.src_point, src_stonecolor)
+                print(
+                    f"新規　 src({trident.src_point}) src_stonecolor:{src_stonecolor}")
+
+                GrundyGraph.__make_each_tridents_from(
+                    ins, drawing_columns, drawing_rows, a, b, c, ha, hb, hc,
+                    trident.a_point, tp_table, nim_constants.stonecolor_a, src_color_table)
+                """a点から生えている三本毛"""
+
+                GrundyGraph.__make_each_tridents_from(
+                    ins, drawing_columns, drawing_rows, a, b, c, ha, hb, hc,
+                    trident.b_point, tp_table, nim_constants.stonecolor_b, src_color_table)
+                """b点から生えている三本毛"""
+
+                GrundyGraph.__make_each_tridents_from(
+                    ins, drawing_columns, drawing_rows, a, b, c, ha, hb, hc,
+                    trident.c_point, tp_table, nim_constants.stonecolor_c, src_color_table)
+                """c点から生えている三本毛"""
+            else:
+                """存在する三本毛なら"""
+                exist_src_stonecolor = src_color_table.get_stonecolor(
+                    trident.src_point)
+                if exist_src_stonecolor < src_stonecolor:
+                    """上書きできる石の色なら"""
+                    src_color_table.add_stonecolor(
+                        trident.src_point, src_stonecolor)  # Update
+                    print(
+                        f"上書き src({trident.src_point}) exist_src_stonecolor:{exist_src_stonecolor} src_stonecolor:{src_stonecolor}")
+                else:
+                    print(
+                        f"無視　 src({trident.src_point}) exist_src_stonecolor:{exist_src_stonecolor} src_stonecolor:{src_stonecolor}")
 
     def __init__(self):
         self.__tp_table = TranspositionTable()
@@ -23,6 +90,3 @@ class GrundyGraph:
     @property
     def root_point(self):
         return self.__root_point
-
-    def setup():
-        pass
--- me/ideas/transfer_map_spin/s_a_b_c_image_gen.py
+++ me/ideas/transfer_map_spin/s_a_b_c_image_gen.py
@@ -10,7 +10,6 @@ import cv2
 import numpy as np
 from kernel.math.eo_code import EoCode
 from kernel.math.music_chord import MusicChord
-from ideas.transfer_map_spin.trident_hair import TridentHair
 from ideas.transfer_map_spin.grundy_graph import GrundyGraph
 from ideas.transfer_map_spin.nim_constants import nim_constants
 
@@ -103,10 +102,15 @@ def gen_s_a_b_c_image(a, b, c, zoom=1.0, is_temporary=True):
                          monochrome_color, dtype=np.uint8)
 
         # モデル作成
-        grundy_graph = GrundyGraph()
-
-        make_each_tridents_from(
-            grundy_graph.root_point, grundy_graph.tp_table, nim_constants.stonecolor_x, grundy_graph.src_color_table)
+        grundy_graph = GrundyGraph.make(
+            drawing_columns=drawing_columns,
+            drawing_rows=drawing_rows,
+            a=a,
+            b=b,
+            c=c,
+            ha=ha,
+            hb=hb,
+            hc=hc)
 
         draw_subtraction_set(canvas, (0, 0))
         """サブストラクションセット描画"""
@@ -132,57 +136,6 @@ def gen_s_a_b_c_image(a, b, c, zoom=1.0, is_temporary=True):
         cv2.imwrite(
             f"./output_tmp/transfer_map_spin_s_{a:02}_{b:02}_{c:02}_{eo_code}{music_chord_text}{tmp_text}.png", canvas)
         """画像出力"""
-
-    def make_each_tridents_from(src_point, tp_table, src_stonecolor, src_color_table):
-        trident = TridentHair.make(
-            src_point,
-            columns=drawing_columns,
-            rows=drawing_rows,
-            a=a,
-            b=b,
-            c=c,
-            ha=ha,
-            hb=hb,
-            hc=hc)
-
-        if trident is not None:
-            """指定の範囲内のみ描画"""
-
-            hash_key = trident.create_hash()
-            print(f"src({trident.src_point}) hash_key:{hash_key}")
-
-            if not tp_table.contains_key(hash_key):
-                """存在しない三本毛なら登録"""
-                tp_table.add_trident(hash_key, trident)
-                src_color_table.add_stonecolor(
-                    trident.src_point, src_stonecolor)
-                print(
-                    f"新規　 src({trident.src_point}) src_stonecolor:{src_stonecolor}")
-
-                make_each_tridents_from(
-                    trident.a_point, tp_table, nim_constants.stonecolor_a, src_color_table)
-                """a点から生えている三本毛"""
-
-                make_each_tridents_from(
-                    trident.b_point, tp_table, nim_constants.stonecolor_b, src_color_table)
-                """b点から生えている三本毛"""
-
-                make_each_tridents_from(
-                    trident.c_point, tp_table, nim_constants.stonecolor_c, src_color_table)
-                """c点から生えている三本毛"""
-            else:
-                """存在する三本毛なら"""
-                exist_src_stonecolor = src_color_table.get_stonecolor(
-                    trident.src_point)
-                if exist_src_stonecolor < src_stonecolor:
-                    """上書きできる石の色なら"""
-                    src_color_table.add_stonecolor(
-                        trident.src_point, src_stonecolor)  # Update
-                    print(
-                        f"上書き src({trident.src_point}) exist_src_stonecolor:{exist_src_stonecolor} src_stonecolor:{src_stonecolor}")
-                else:
-                    print(
-                        f"無視　 src({trident.src_point}) exist_src_stonecolor:{exist_src_stonecolor} src_stonecolor:{src_stonecolor}")
 
     def draw_subtraction_set(canvas, point):
         """サブトラクションセットを表示"""
