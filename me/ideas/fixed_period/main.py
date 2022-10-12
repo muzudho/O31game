4c6ad2551be8ca2464257e78c61818681086bcac
むずでょ <muzudho1@gmail.com>
2022-10-12 23:44:50 +0900

    p

---
+++ me/ideas/fixed_period/main.py
@@ -0,0 +1,23 @@
+"""
+pip install opencv-python
+pip install Pillow
+
+cd me
+
+python.exe -m ideas.fixed_period.main
+"""
+import random
+from ideas.fixed_period.s_a_b_c_p_image_gen import gen_s_a_b_c_p_image
+
+zoom = 0.25
+"""倍率。1倍はかなりでかい"""
+
+
+def main():
+    # Clouds passer
+    gen_s_a_b_c_p_image(a=1, b=2, c=3, p=4,
+                        zoom=1.0, is_temporary=False)
+
+
+if __name__ == "__main__":
+    main()
